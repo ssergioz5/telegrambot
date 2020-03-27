eda92837e0c1954bef0411e64ec4aeab69655aa1
vscode <xssergioz5x@gmail.com>
2020-03-27 20:07:04 +0100

    vscode

--- botsergio.py
+++ botsergio.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hola!')
 
 
 def help(update, context):
